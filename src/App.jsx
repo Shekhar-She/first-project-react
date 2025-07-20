4febefc58dbae1fce7cfa867ce49196dd0265e8f
Shekhar-She <shekhararmy1@gmail.com>
2025-07-20 12:40:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ function app(){
   return<div>
   <h1> This is the beat React Course</h1>
   <button>Like This Buttion</button>
-  
+  <p>
+    hello 
+  </p>
   <Hello> </Hello>
    </div>
+  
+
 
 }
 export default app;
